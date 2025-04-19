2204a995535f7191e58636b2a8ef0c91eec2bf89
metafates <vvz.dev@gmail.com>
2025-04-19 13:44:08 +0300

    remove internal bench package

--- bench/bench_test.go
+++ bench/bench_test.go
@@ -5,27 +5,82 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/metafates/schema/internal/bench"
+	"github.com/metafates/schema/required"
 	"github.com/metafates/schema/validate"
 )
 
-//go:generate schemagen -type A
-type A struct{}
-
 //go:embed testdata.json
 var testdata []byte
+
+//go:generate schemagen -type DataWithGen
+
+type DataWithGen []struct {
+	ID         required.NonZero[string]    `json:"_id"`
+	Index      int                         `json:"index"`
+	GUID       string                      `json:"guid"`
+	IsActive   bool                        `json:"isActive"`
+	Balance    string                      `json:"balance"`
+	Picture    string                      `json:"picture"`
+	Age        required.Positive[int]      `json:"age"`
+	EyeColor   string                      `json:"eyeColor"`
+	Name       string                      `json:"name"`
+	Gender     string                      `json:"gender"`
+	Company    string                      `json:"company"`
+	Email      string                      `json:"email"`
+	Phone      string                      `json:"phone"`
+	Address    string                      `json:"address"`
+	About      string                      `json:"about"`
+	Registered string                      `json:"registered"`
+	Latitude   required.Latitude[float64]  `json:"latitude"`
+	Longitude  required.Longitude[float64] `json:"longitude"`
+	Tags       []string                    `json:"tags"`
+	Friends    []struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"friends"`
+	Greeting      string `json:"greeting"`
+	FavoriteFruit string `json:"favoriteFruit"`
+}
+
+type Data []struct {
+	ID         required.NonZero[string]    `json:"_id"`
+	Index      int                         `json:"index"`
+	GUID       string                      `json:"guid"`
+	IsActive   bool                        `json:"isActive"`
+	Balance    string                      `json:"balance"`
+	Picture    string                      `json:"picture"`
+	Age        required.Positive[int]      `json:"age"`
+	EyeColor   string                      `json:"eyeColor"`
+	Name       string                      `json:"name"`
+	Gender     string                      `json:"gender"`
+	Company    string                      `json:"company"`
+	Email      string                      `json:"email"`
+	Phone      string                      `json:"phone"`
+	Address    string                      `json:"address"`
+	About      string                      `json:"about"`
+	Registered string                      `json:"registered"`
+	Latitude   required.Latitude[float64]  `json:"latitude"`
+	Longitude  required.Longitude[float64] `json:"longitude"`
+	Tags       []string                    `json:"tags"`
+	Friends    []struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"friends"`
+	Greeting      string `json:"greeting"`
+	FavoriteFruit string `json:"favoriteFruit"`
+}
 
 func BenchmarkUnmarshalJSON(b *testing.B) {
 	b.Run("reflection", func(b *testing.B) {
 		b.Run("with validation", func(b *testing.B) {
 			for b.Loop() {
-				var w bench.Data
+				var data Data
 
-				if err := json.Unmarshal(testdata, &w); err != nil {
+				if err := json.Unmarshal(testdata, &data); err != nil {
 					b.Fatal(err)
 				}
 
-				if err := validate.Validate(&w); err != nil {
+				if err := validate.Validate(&data); err != nil {
 					b.Fatal(err)
 				}
 			}
@@ -33,9 +88,9 @@ func BenchmarkUnmarshalJSON(b *testing.B) {
 
 		b.Run("without validation", func(b *testing.B) {
 			for b.Loop() {
-				var w bench.Data
+				var data Data
 
-				if err := json.Unmarshal(testdata, &w); err != nil {
+				if err := json.Unmarshal(testdata, &data); err != nil {
 					b.Fatal(err)
 				}
 			}
@@ -45,13 +100,13 @@ func BenchmarkUnmarshalJSON(b *testing.B) {
 	b.Run("codegen", func(b *testing.B) {
 		b.Run("with validation", func(b *testing.B) {
 			for b.Loop() {
-				var w bench.DataWithGen
+				var data DataWithGen
 
-				if err := json.Unmarshal(testdata, &w); err != nil {
+				if err := json.Unmarshal(testdata, &data); err != nil {
 					b.Fatal(err)
 				}
 
-				if err := validate.Validate(&w); err != nil {
+				if err := validate.Validate(&data); err != nil {
 					b.Fatal(err)
 				}
 			}
@@ -59,9 +114,9 @@ func BenchmarkUnmarshalJSON(b *testing.B) {
 
 		b.Run("without validation", func(b *testing.B) {
 			for b.Loop() {
-				var w bench.DataWithGen
+				var data DataWithGen
 
-				if err := json.Unmarshal(testdata, &w); err != nil {
+				if err := json.Unmarshal(testdata, &data); err != nil {
 					b.Fatal(err)
 				}
 			}
