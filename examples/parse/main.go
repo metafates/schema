0490531fc1a0072d0f9c80011d86d9ae1c405611
metafates <vvz.dev@gmail.com>
2025-04-23 21:49:50 +0300

    better parse example

--- examples/parse/main.go
+++ examples/parse/main.go
@@ -5,46 +5,87 @@ import (
 	"log"
 	"time"
 
+	schemajson "github.com/metafates/schema/encoding/json"
 	"github.com/metafates/schema/optional"
 	"github.com/metafates/schema/parse"
 	"github.com/metafates/schema/required"
 	"github.com/metafates/schema/validate/charset"
 )
 
+// Let's assume we have the following structure
 type User struct {
-	Name required.Charset[string, charset.Print]
-
+	ID    required.UUID[string]
+	Name  required.Charset[string, charset.Print]
 	Birth optional.InPast[time.Time]
 
-	Bio string
+	FavoriteNumber int
+
+	Friends []Friend
+}
+
+type Friend struct {
+	ID   required.UUID[string]
+	Name required.Charset[string, charset.Print]
 }
 
 func main() {
+	// We can use json to unmarshal data to it
 	{
 		var user User
 
-		err := parse.Parse(&user, map[string]any{
-			"Name":  "john",
-			"Birth": time.Date(1900, time.September, 10, 0, 0, 0, 0, time.UTC),
-			"Bio":   "...",
-		})
-		if err != nil {
+		const data = `
+		{
+			"ID": "2c376d16-321d-43b3-8648-2e64798cc6b3",
+			"Name": "john",
+			"FavoriteNumber": 42,
+			"Friends": [
+				{"ID": "7f735045-c8d2-4a60-9184-0fc033c40a6a", "Name": "jane"}
+			]
+		}
+		`
+
+		if err := schemajson.Unmarshal([]byte(data), &user); err != nil {
 			log.Fatalln(err)
 		}
 
-		fmt.Println(user.Name.Get())
-		// john
+		fmt.Println(user.Friends[0].ID.Get()) // 7f735045-c8d2-4a60-9184-0fc033c40a6a
 	}
 
-	{
+	// But we can also construct our user manually through parsing!
+	//
+	// For example, we can have some data as map.
+	// Note, that field names should match go names exactly. Field tags (`json:"myField"`) won't be considered
+	dataAsMap := map[string]any{
+		"ID":             "2c376d16-321d-43b3-8648-2e64798cc6b3",
+		"Name":           "john",
+		"FavoriteNumber": 42,
+		"Friends": []map[string]any{
+			{"ID": "7f735045-c8d2-4a60-9184-0fc033c40a6a", "Name": "jane"},
+		},
+	}
+
+	// structs are also supported. It is ok to omit non-required fields
+	dataAsStruct := struct {
+		ID             string
+		Name           []byte // types will be converted, if possible. If not - error is returned
+		FavoriteNumber uint16
+	}{
+		ID:             "2c376d16-321d-43b3-8648-2e64798cc6b3",
+		Name:           []byte("john"),
+		FavoriteNumber: 42,
+	}
+
+	// Now let's parse it
+	for _, data := range []any{
+		dataAsMap,
+		dataAsStruct,
+	} {
 		var user User
 
-		err := parse.Parse(&user, struct{ Name string }{Name: "john"})
-		if err != nil {
+		if err := parse.Parse(&user, data); err != nil {
 			log.Fatalln(err)
 		}
 
-		fmt.Println(user.Name.Get())
-		// john
+		fmt.Println(user.ID.Get()) // 2c376d16-321d-43b3-8648-2e64798cc6b3
 	}
 }
